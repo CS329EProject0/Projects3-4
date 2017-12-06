07728e7fd33319dac7e5f2ed13e6737fcadbdf9d
nmosley3 <31999154+nmosley3@users.noreply.github.com>
2017-12-06 17:02:46 -0600

    Added views and urls for most pages

--- whiteboard/canvas/urls.py
+++ whiteboard/canvas/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
    #url(r'featured/', Featured.as_view()), #This is a broken url
    url(r'welcome/', WelcomeView.as_view(), name='welcome'),
    url(r'register/', UserFormView.as_view(), name="register"),
-   url(r'login/', auth_views.login, {'template_name':'login.html'}, name="login"),
+   url(r'login/', auth_views.login, {'template_name':'login.html'}),
+   url(r'create_quiz/', QuizCreationView.as_view(),name='quizCreation')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
