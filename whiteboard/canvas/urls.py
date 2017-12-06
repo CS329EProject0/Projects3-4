f5a751d7aed94f51b14e7dc52a47cb51e4f1ccb9
benjaminmaccini <benjaminmaccini@gmail.com>
2017-12-06 16:26:36 -0600

    Url resolver fixed

--- whiteboard/canvas/urls.py
+++ whiteboard/canvas/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
    #url(r'featured/', Featured.as_view()), #This is a broken url
    url(r'welcome/', WelcomeView.as_view(), name='welcome'),
    url(r'register/', UserFormView.as_view(), name="register"),
-   url(r'login/', auth_views.login, {'template_name':'login.html'}),
+   url(r'login/', auth_views.login, {'template_name':'login.html'}, name="login"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
